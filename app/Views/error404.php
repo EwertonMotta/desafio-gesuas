aa6de013b68ded1a3a8fa9900f65e63546991cf6
Ewerton Motta <ewerton.emotta@gmail.com>
2023-09-05 13:37:29 -0300

    Adicionando o layout a view 404

--- app/Views/error404.php
+++ app/Views/error404.php
@@ -1,4 +1,8 @@
-<div>
-    <h1>404</h1>
-    <h2>Página não encontrada...</h2>
-</div>
+<?php require_once PATH_BASE . '/app/Views/layout/header.php'; ?>
+    <div class="container d-flex justify-content-center">
+        <div class="text-center">
+            <h1>404</h1>
+            <h2>Página não encontrada...</h2>
+        </div>
+    </div>
+<?php require_once PATH_BASE . '/app/Views/layout/footer.php'; ?>
